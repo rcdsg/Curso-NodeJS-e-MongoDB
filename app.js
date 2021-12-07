1df89b4ba18642ff24134592d20d8b488af2d538
Rodrigo Costa <rodrigokosta@gmail.com>
2021-12-06 22:58:59 -0300

    setup consign for routes

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const app = require('./config/server');
-const rotaNoticias = require('./app/routes/noticias')(app);
-const rotaHome = require('./app/routes/home')(app);
-const rotaFormNot = require('./app/routes/formNoticia')(app);
+
 
 app.listen(4000, function(req,res){
     console.log('Servidor rodando na porta 4000!');
