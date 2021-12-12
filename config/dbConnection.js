802bada4d2f6ed5ce26d42c19e2672b70ecbee38
Rodrigo Costa <rodrigokosta@gmail.com>
2021-12-12 17:34:36 -0300

    refactoring for dbconnection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,15 @@
 const mySql = require('mysql');
 
-module.exports = function(){
+const connectMySQL = function(){
+    console.log('Conexão com bd estabelecida com sucesso!');
     return mySql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'mSql@200822',
         database: 'portal_noticias'
     });
+};
+
+module.exports = function(){
+    return connectMySQL;
 }
